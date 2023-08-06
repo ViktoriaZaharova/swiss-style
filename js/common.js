f282cf19eab292414abcd1f38c51b1014e978075
Виктория Захарова <zaharova181114@gmail.com>
2023-08-06 23:53:08 +0300

    first commit

---
+++ js/common.js
@@ -0,0 +1,9 @@
+$('[name="phone"]').mask('+7 (999) 999-99-99');
+
+$('.home-slider').slick({
+	slidesToShow: 1,
+	dots: true,
+	fade: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+})
