9d901afcf2febb3b2ad124e6ecdc6b0b975ba64a
Виктория Захарова <zaharova181114@gmail.com>
2023-08-08 18:16:23 +0300

    add section all

--- js/common.js
+++ js/common.js
@@ -6,4 +6,23 @@ $('.home-slider').slick({
 	fade: true,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
-})
+});
+
+$('.products-slider').slick({
+	slidesToShow: 6,
+	slidesToScroll: 2,
+	arrows: false,
+	dots: true,
+});
+
+$('[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+	$('.slick-slider').slick('setPosition');
+});
+
+$('.categories-products-slider').slick({
+	slidesToShow: 1,
+	centerMode: true,
+	variableWidth: true,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
