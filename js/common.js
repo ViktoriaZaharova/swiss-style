cbfb0cf3b04686f3f7347e707064a9b15c4fff0c
Виктория Захарова <zaharova181114@gmail.com>
2023-08-11 22:27:57 +0300

    edit slider

--- js/common.js
+++ js/common.js
@@ -19,10 +19,58 @@ $('[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
 	$('.slick-slider').slick('setPosition');
 });
 
-$('.categories-products-slider').slick({
-	slidesToShow: 1,
-	centerMode: true,
-	variableWidth: true,
+// $('.categories-products-slider').slick({
+// 	slidesToShow: 1,
+// 	centerMode: true,
+// 	variableWidth: true,
+// 	// infinite: false,
+// 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+// 	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+// });
+
+var rev = $(".categories-products-slider");
+rev
+	.on("init", function (event, slick, currentSlide) {
+		var cur = $(slick.$slides[slick.currentSlide]),
+			next = cur.next(),
+			prev = cur.prev();
+		prev.addClass("slick-sprev");
+		next.addClass("slick-snext");
+		cur.removeClass("slick-snext").removeClass("slick-sprev");
+		slick.$prev = prev;
+		slick.$next = next;
+	})
+	.on("beforeChange", function (event, slick, currentSlide, nextSlide) {
+		console.log("beforeChange");
+		var cur = $(slick.$slides[nextSlide]);
+		console.log(slick.$prev, slick.$next);
+		slick.$prev.removeClass("slick-sprev");
+		slick.$next.removeClass("slick-snext");
+		(next = cur.next()), (prev = cur.prev());
+		prev.prev();
+		prev.next();
+		prev.addClass("slick-sprev");
+		next.addClass("slick-snext");
+		slick.$prev = prev;
+		slick.$next = next;
+		cur.removeClass("slick-next").removeClass("slick-sprev");
+	});
+
+rev.slick({
+	speed: 1000,
+	arrows: true,
+	dots: false,
+	focusOnSelect: true,
 	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
-	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>',
+	infinite: true,
+	centerMode: true,
+	slidesPerRow: 1,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	centerPadding: "0",
+	swipe: true,
+	customPaging: function (slider, i) {
+		return "";
+	}
 });
