935826808a8a0740042fc995175f66c9a1377aac
Виктория Захарова <zaharova181114@gmail.com>
2023-09-08 13:59:43 +0300

    add catalog

--- js/common.js
+++ js/common.js
@@ -19,6 +19,21 @@ $('[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
 	$('.slick-slider').slick('setPosition');
 });
 
+$('.articles-slider').slick({
+	slidesToShow: 5,
+	slidesToScroll: 2,
+	arrows: false,
+	dots: true,
+});
+
+$('.brands-slider').slick({
+	slidesToShow: 8,
+	variableWidth: true,
+	infinite: false,
+	prevArrow: '<button type="button" class="slick-prev"><svg class="svg-icon"><use xlink:href="img/sprite.svg#prev"></use></svg></button>',
+	nextArrow: '<button type="button" class="slick-next"><svg class="svg-icon"><use xlink:href="img/sprite.svg#next"></use></svg></button>'
+});
+
 // $('.categories-products-slider').slick({
 // 	slidesToShow: 1,
 // 	centerMode: true,
@@ -85,3 +100,136 @@ $('.sidebar-box__menu li a').each(function () {
 	}
 });
 // end
+
+
+// disabled button form cabinet
+$(document).ready(function () {
+	$('.form-cabinet [type="submit"]').attr('disabled', 'disabled');
+	$('.form-cabinet input').on('input', function () {
+		if ($(this).val() !== '') {
+			$('.form-cabinet [type="submit"]').removeAttr("disabled");
+		} else {
+			$('.form-cabinet [type="submit"]').attr('disabled', 'disabled');
+		}
+	});
+});
+
+// text hidden
+$('.text-toggle').each(function () {
+	if ($(this).height() > 113) {
+		$(this).addClass('hide');
+		$(this).parent('.text-toggle-wrapper').append('<a href="#" class="load-more-wrap"><span>Читать полностью</span><svg class="svg-icon"><use xlink:href="img/sprite.svg#arrow-right"></use></svg></a>');
+	}
+});
+
+$('.load-more-wrap').on('click', function (e) {
+	e.preventDefault();
+
+	var
+		$this = $(this),
+		content = $(this).prev('.text-toggle');
+
+	if (!$this.hasClass('trigger')) {
+		$this.addClass('trigger');
+		$this.find('span').html('Свернуть');
+
+		content.removeClass('hide');
+	} else {
+		$this.removeClass('trigger');
+		$this.find('span').html('Читать полностью');
+
+		content.addClass('hide');
+	}
+});
+// text hidden
+
+// hidden list > 5
+$('.accordion-catalog').each(function () {
+	if ($(this).find('.accordion-item').length > 10) {
+		$(this).find('.accordion-item').slice(10).hide();
+		$(this).append('<a href="#" class="link-all-filter color-inherit text-decoration">Все фильтры +</a>');
+	}
+
+});
+
+// hidden list > 5
+
+// show list all
+$('.link-all-filter').on('click', function (e) {
+	e.preventDefault();
+	$(this).prev('.accordion-item:hidden').slice(0, 5).slideDown();
+
+	var onBlock = $(this).prev('.accordion-item:hidden').length;
+	if (onBlock <= 0) {
+		$(this).hide();
+	}
+});
+
+// show list all
+
+// slider range
+$(document).ready(function () {
+	$('.slider-range').slider({
+		range: true,
+		min: 5999,
+		max: 1000000,
+		values: [5999, 640000],
+		animate: "fast",
+		slide: function (event, ui) {
+			$(".dec1").val(ui.values[0]);
+			$(".dec2").val(ui.values[1]);
+		}
+	});
+
+	$(".dec1").val($(".slider-range").slider("values", 0));
+	$(".dec2").val($(".slider-range").slider("values", 1));
+	$(".range_val input").change(function () {
+		var input_left = $(".dec1").val().replace(/[^0-9]/g, ''),
+			opt_left = $(".slider-range").slider("option", "min"),
+			where_right = $(".slider-range").slider("values", 1),
+			input_right = $(".dec2").val().replace(/[^0-9]/g, ''),
+			opt_right = $(".slider-range").slider("option", "max"),
+			where_left = $(".slider-range").slider("values", 0);
+		if (input_left > where_right) {
+			input_left = where_right;
+		}
+		if (input_left < opt_left) {
+			input_left = opt_left;
+		}
+		if (input_left == "") {
+			input_left = 0;
+		}
+		if (input_right < where_left) {
+			input_right = where_left;
+		}
+		if (input_right > opt_right) {
+			input_right = opt_right;
+		}
+		if (input_right == "") {
+			input_right = 0;
+		}
+		$(".dec1").val(input_left);
+		$(".dec2").val(input_right);
+		if (input_left != where_left) {
+			$(".slider-range").slider("values", 0, input_left);
+		}
+		if (input_right != where_right) {
+			$(".slider-range").slider("values", 1, input_right);
+		}
+	});
+
+});
+
+
+// password view
+$('.btn-view-password').on('click', function () {
+	if ($(this).siblings('.input-password').attr('type') === 'password') {
+		$(this).addClass('view');
+		$(this).siblings('.input-password').attr('type', 'text');
+	} else {
+		$(this).removeClass('view');
+		$(this).siblings('.input-password').attr('type', 'password');
+	}
+	return false;
+});
+
