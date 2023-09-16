c34ebd83c9e1e05336eef668cde6e3a927c5b583
Виктория Захарова <zaharova181114@gmail.com>
2023-09-16 17:46:33 +0300

    all desktop

--- js/common.js
+++ js/common.js
@@ -327,6 +327,40 @@ $('.link-details-order').on('click', function (e) {
 	}
 });
 
+// active menu article
+$(document).ready(function () {
+	var sections = $('.section'),
+		nav = $('.article-navigation .sidebar-box__menu'),
+		nav_height = nav.outerHeight();
+
+	$(window).on('scroll', function () {
+		var cur_pos = $(this).scrollTop();
+
+		sections.each(function () {
+			var top = $(this).offset().top - nav_height,
+				bottom = top + $(this).outerHeight();
+
+			if (cur_pos >= top && cur_pos <= bottom) {
+				nav.find('a').removeClass('active');
+				sections.removeClass('active');
+
+				$(this).addClass('active');
+				nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+			}
+		});
+	});
+
+	nav.find('a').on('click', function () {
+		var $el = $(this)
+			, id = $el.attr('href');
+
+		$('html, body').animate({
+			scrollTop: $(id).offset().top - nav_height
+		}, 500);
+
+		return false;
+	});
+});
 
 Fancybox.bind("[data-fancybox]", {
 	// Your custom options
